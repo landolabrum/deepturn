2356a81e2c435a28c57375a5713261811f01dc2e
landolabrum <lando@deepturn.com>
2023-11-13 06:26:38 +0000

    lando 13.11.23-06:26:38

--- src/modules/shared/components/Navbar/controller/Navbar.tsx
+++ src/modules/shared/components/Navbar/controller/Navbar.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState, useMemo } from "react";
 import styles from "./Navbar.scss";
 import { UiIcon } from "@webstack/components/UiIcon/UiIcon";
@@ -6,90 +5,79 @@ import { IRoute, useClearanceRoutes } from "../data/routes";
 import useWindow from "@webstack/hooks/useWindow";
 import useRoute from "~/src/core/authentication/hooks/useRoute";
 import UiButton from "@webstack/components/UiButton/UiButton";
-import environment from "~/src/environment";
 import Authentication from "~/src/pages/authentication";
 import { useCartTotal } from "~/src/modules/ecommerce/cart/hooks/useCart";
 import { useModal } from "@webstack/components/modal/contexts/modalContext";
 import UiSelect from "@webstack/components/UiSelect/UiSelect";
-import { useRouter } from "next/router";
-const Navbar = () => {
-  const MobileNav = ({routes}:{routes:IRoute[]}) => {
-    const handleMobileClick = (selectedRoute: IRoute) => {
-      console.log('[ HANDLE Mobile CLICK ]', selectedRoute);
-      if(selectedRoute.items){
-        return <MobileNav routes={selectedRoute.items}/>;
-      }
-      // Implement your logic here
-      // Example: if it's a string route, navigate to it
-      if (typeof selectedRoute.href === 'string') {
-        // Navigate to the route
-        // Example: router.push(selectedRoute.href);
-      }
-      // Add any additional logic you need for modal or other route types
-    };
 
-    return (
-      <>
-        <style jsx>{styles}</style>
-        <div className='navbar__mobile'>
-          {routes && routes.map((route,key) => (
-            <div
-              key={key}
-              className={`nav__nav-item nav__nav-item--${route?.label}`}
-              onClick={() => handleMobileClick(route)}
-            >
-              <div>
-                <div className='nav__nav-item__label'>
-                  {route.label}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </>
-    )
-  }
+const Navbar = () => {
   const width = useWindow().width;
   const [user, route, setRoute]: any = useRoute();
   const routes = useClearanceRoutes();
   const { openModal, closeModal, isModalOpen } = useModal();
+  const [currentRoutes, setCurrentRoutes] = useState(routes); // State for current routes in mobile view
+
+  // Define modals for different routes
   const modals: any = {
     login: <Authentication />
-  }
+  };
 
+  // Handle mobile navigation click
+  const handleMobileClick = (selectedRoute: IRoute) => {
+    if (selectedRoute.items) {
+      setCurrentRoutes(selectedRoute.items);
+    } else {
+      // Handle other click scenarios
+    }
+  };
+
+  // Function to go back to the original routes in mobile view
+  const handleBackButtonClick = () => {
+    setCurrentRoutes(routes);
+  };
+
+  // Handle click events for routes and modals
   const handleClick = (route: any) => {
-    console.log('[ HANDLE CLICK ]', Boolean(route?.modal), isModalOpen);
-
-    // If it's a string route, just set the route
     if (typeof route === 'string') {
       setRoute(route);
-    } else if (route?.modal) {
-        return openModal(modals[route.modal]);
-    }
-    if (isModalOpen) {
+    } else if (route?.modal && !isModalOpen) {
       openModal(modals[route.modal]);
     }
   };
 
+  // Toggle mobile navigation or modal
   const handleTrigger = () => {
-    if (!isModalOpen) {
-      openModal(<MobileNav routes={routes}/>);
-    } else {
-      closeModal();
-    }
+    isModalOpen ? closeModal() : openModal(<MobileNav routes={routes} onBackButtonClick={handleBackButtonClick} />);
   };
 
+  // Compute user display name
   const displayName = useMemo(() => {
-    if (user) {
-      const [firstName, lastName] = user.name.split(" ");
-      return `${firstName} ${lastName.substring(0, 1)}.`;
-    }
-    return "";
+    return user ? `${user.name.split(" ")[0]} ${user.name.split(" ")[1][0]}.` : "";
   }, [user]);
 
-  // useEffect(() => {}, [isModalOpen]);
   const cartTotal = useCartTotal();
-  const cartRoute = routes.find((r: any) => r.href == '/cart');
+  const cartRoute = routes.find((r: any) => r.href === '/cart');
+
+  // Mobile navigation component
+  const MobileNav = ({ routes }) => (
+    <>
+      <style jsx>{styles}</style>
+      <div className='navbar__mobile'>
+        {routes.map((route: IRoute, key: number) => (
+          <div
+            key={key}
+            className={`nav__nav-item nav__nav-item--${route?.label}`}
+            onClick={() => handleMobileClick(route)}
+          >
+            <div className='nav__nav-item__label'>
+              {route.label}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+
   return (
     <>
       <style jsx>{styles}</style>
@@ -100,35 +88,33 @@ const Navbar = () => {
             onClick={handleTrigger}
           />
         </div>
-        {routes &&
-          Object.entries(routes).map(([key, route]) => {
-            return <div
-              key={key}
-              className={`nav__nav-item nav__nav-item--${route?.label}`}
-              onDoubleClick={() =>
-                route?.href && handleClick({ href: route.href })}
-            >
-              {!route?.items && <UiButton
+        {routes.map((route, key) => (
+          <div
+            key={key}
+            className={`nav__nav-item nav__nav-item--${route?.label}`}
+            onDoubleClick={() => route?.href && handleClick({ href: route.href })}
+          >
+            {!route?.items ? (
+              <UiButton
                 variant='flat'
-                onClick={() => handleClick(route)}>
+                onClick={() => handleClick(route)}
+              >
                 {route.label}
-              </UiButton>}
-              {route?.items && <UiSelect
+              </UiButton>
+            ) : (
+              <UiSelect
                 variant='nav-item'
-                value={
-                  route.label == 'account' ? displayName : route.label
-                }
+                value={route.label === 'account' ? displayName : route.label}
                 options={route?.items}
                 onSelect={handleClick}
-              >
-              </UiSelect>}
-            </div>
-          }
-          )
-        }
+              />
+            )}
+          </div>
+        ))}
+        <MobileNav routes={currentRoutes} />
       </nav>
-      <MobileNav routes={routes}/>
     </>
   );
 };
+
 export default Navbar;
