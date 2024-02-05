64cf30fb652e586130da14aaa80c4037c81931ed
landolabrum <lando@deepturn.com>
2024-02-05 10:31:02 +0000

    Deploy to gh-pages

---
+++ out/_next/static/chunks/pages/admin/[view]-7116930310975fd7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{92719:function(n,u,_){"use strict";_.r(u);var i=_(50504);u.default=i.Z},9563:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/[view]",function(){return _(92719)}])}},function(n){n.O(0,[443,847,895,828,504,888,774,179],function(){return n(n.s=9563)}),_N_E=n.O()}]);
