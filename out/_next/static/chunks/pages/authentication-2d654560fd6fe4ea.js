5082fcf18ca71d61769a1f27b32f4965f1c14cb0
landolabrum <lando@deepturn.com>
2024-03-04 08:15:21 +0000

    lando 04.03.24-08:15:21

--- out/_next/static/chunks/pages/authentication-2d654560fd6fe4ea.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[999],{6481:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/authentication",function(){return u(688)}])}},function(n){n.O(0,[2888,9774,179],function(){return n(n.s=6481)}),_N_E=n.O()}]);
--- out/_next/static/chunks/pages/configure-50cc9e86ba2e2e2a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6202],{4188:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/configure",function(){return u(4317)}])}},function(n){n.O(0,[4317,2888,9774,179],function(){return n(n.s=4188)}),_N_E=n.O()}]);
