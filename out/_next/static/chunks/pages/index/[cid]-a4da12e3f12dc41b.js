ca589757dccb8e4b5953800585607e5836b83608
landolabrum <lando@deepturn.com>
2024-04-29 03:19:25 +0000

    Deploy to gh-pages

---
+++ out/_next/static/chunks/pages/index/[cid]-a4da12e3f12dc41b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7845],{67716:function(n,u,_){"use strict";_.r(u);var i=_(64049);u.default=i.Z},65261:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/index/[cid]",function(){return _(67716)}])}},function(n){n.O(0,[2634,5675,4592,5610,4049,2888,9774,179],function(){return n(n.s=65261)}),_N_E=n.O()}]);
