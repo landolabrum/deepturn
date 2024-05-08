25df822ded2cebc918b8d5dca280042890ef63d0
landolabrum <lando@deepturn.com>
2024-05-08 13:28:25 +0000

    lando 08.05.24-13:28:25

--- src/environment.ts
+++ src/environment.ts
@@ -6,7 +6,7 @@ import { IEnvironment } from "./core/environments/environment.interface";
 let environment: IEnvironment = devEnvironment;
 
 // const DEV_URL: string = "https://tiktok.soy"
-const DEV_URL: string = "https://localhost:3000"
+const DEV_URL: string = "http://localhost:3000"
 
 export const isEnvironmentProduction = () => {
     if(typeof window == "object")return window.location.href?.toLowerCase().includes(DEV_URL);
