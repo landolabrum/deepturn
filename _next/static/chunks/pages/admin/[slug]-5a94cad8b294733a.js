b5f94510f0931f2dc6be0048d635927fd248ef59
landolabrum <lando@deepturn.com>
2024-06-23 08:07:53 +0000

    Updates

--- _next/static/chunks/pages/admin/[slug]-5a94cad8b294733a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[815],{71370:function(n,u,_){"use strict";_.r(u);var t=_(53451);u.default=t.Z},22276:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/[slug]",function(){return _(71370)}])}},function(n){n.O(0,[5675,4592,1695,3451,2888,9774,179],function(){return n(n.s=22276)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/index/index/Index-f61b25432645c913.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5635],{73392:function(n,u,_){"use strict";_.r(u);var e=_(6975);u.default=e.Z},13601:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/index/Index",function(){return _(73392)}])}},function(n){n.O(0,[2634,5675,5190,6005,6975,2888,9774,179],function(){return n(n.s=13601)}),_N_E=n.O()}]);
