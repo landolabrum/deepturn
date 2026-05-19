9b8bafa4f4e669e94183b74116f1ebe5c4252dca
landolabrum <lando@deepturn.com>
2026-05-18 19:26:51 -0600

    deploy 2026-05-19T01:26:50.274Z

---
+++ _next/static/chunks/pages/index/[cid]-a15972fd41ada160.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2263],{8996:(_,e,n)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/index/[cid]",function(){return n(44185)}])},44185:(_,e,n)=>{"use strict";n.r(e),n.d(e,{default:()=>u});let u=n(18919).A}},_=>{_.O(0,[3602,7391,2328,1376,624,7633,3292,8919,636,6593,8792],()=>_(_.s=8996)),_N_E=_.O()}]);
