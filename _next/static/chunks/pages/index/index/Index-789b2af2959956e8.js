79d6238e0c71d4ca21b99f20afd639c971dbc82d
landolabrum <lando@deepturn.com>
2026-06-14 04:29:30 -0600

    deploy 2026-06-14T10:29:29.068Z

---
+++ _next/static/chunks/pages/index/index/Index-789b2af2959956e8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3492],{35928:(_,e,n)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/index/Index",function(){return n(48995)}])},48995:(_,e,n)=>{"use strict";n.r(e),n.d(e,{default:()=>u});let u=n(813).A}},_=>{_.O(0,[3602,7391,6742,7197,624,7633,3292,813,636,6593,8792],()=>_(_.s=35928)),_N_E=_.O()}]);
